819ba4c88aecb3d5ce1b233c17353232df2eb4b7
MEzequielFB <ezequiel_frisone@hotmail.com>
2020-10-20 20:11:07 -0300

    Proyecto ruteado, agregados templates

--- app/controlador/appController.php
+++ app/controlador/appController.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once "appView.php";
+    require_once "app/vista/appView.php";
 
     class appController{
         private $view;
@@ -8,6 +8,18 @@
         function __construct(){
 
             $this->view= new appView();
+        }
+
+        function showIndex(){
+            $this->view->showIndex();
+        }
+
+        function showFemenino(){
+            $this->view->showFemenino();
+        }
+
+        function showMasculino(){
+            $this->view->showMasculino();
         }
 
     }
---
+++ templates/navbar.tpl
@@ -0,0 +1,10 @@
+ <header>
+        <div class="menuDesplegable">MENÚ</div>
+        <nav>
+            <ul class="botonera">
+                <li class="boton"> <a href="{$base_url}">Inicio</a></li>
+                <li class="boton"> <a href="{$base_url}femenino">Fútbol Femenino</a></li>
+                <li class="boton"> <a href="{$base_url}masculino">Fútbol Masculino</a></li>
+            </ul>
+        </nav>
+    </header>
